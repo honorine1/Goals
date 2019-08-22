af8cb29961c578cd3789fed522e968e29895c765
honorine1 <uwurukundoh@gmail.com>
2019-08-22 17:03:27 +0200

    my first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Goal } from './goal';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  // title = 'Goals';
+//   goals: string [];
+//   constructor(){
+// this.goals = ['Watch finding Nemo', 'Buy cookies', 'Get new phone case']
+// }
+// goals: Goal[] = [
+//   {id:1, name:'watch finding Nemo'},
+//   {id:2, name:'Buy cookies'},
+//   {id:3, name:'Get New Phone Case'},
+//   {id:4, name:'Get Dog Food'},
+//   {id:5, name:'Solve math homework'},
+  // {id:6, name:'Plot my world domination plan'},
+// ];
+}
